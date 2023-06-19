29092a23dcc8f6c61356e69fd4cb6e340b17c5bb
mohammad mohsen <mohsenhaidari0766@gmail.com>
2023-06-19 14:45:54 +0430

    commit added because of linter errors

--- classes/item.rb
+++ classes/item.rb
@@ -1,21 +1,21 @@
 class Item
-    attr_accessor :genre, :author, :source, :label, :pulish_date
-    attr_reader :id, :archived
+  attr_accessor :genre, :author, :source, :label, :pulish_date
+  attr_reader :id, :archived
 
-    def initialize(pulish_date, archived: false)
-        @id = rand(1..1000)
-        @pulish_date = pulish_date
-        @archived = archived
-        @labe = nil
-        @genre = nil
-        @author = author
-    end
+  def initialize(pulish_date, archived: false)
+    @id = rand(1..1000)
+    @pulish_date = pulish_date
+    @archived = archived
+    @labe = nil
+    @genre = nil
+    @author = author
+  end
 
-    def can_be_archived?
-        (Time.now.year - @publish_date.year) > 10
-    end
+  def can_be_archived?
+    (Time.now.year - @publish_date.year) > 10
+  end
 
-    def move_to_archive
-        @archived = true if can_be_archived?
-      end
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 end
