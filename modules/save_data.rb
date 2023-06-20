4be2e836c12cf698b7bf90726cc1a83411a3ae3e
FrankMugagga <frank.mugagga@gmail.com>
2023-06-20 21:35:11 +0300

    Create file to save movie data to json file

--- modules/save_data.rb
+++ modules/save_data.rb
@@ -3,6 +3,7 @@ require 'json'
 module SaveData
   def save_data
     save_music_albums
+    save_movie
   end
 
   def save_music_albums
@@ -16,5 +17,18 @@ module SaveData
     end
     File.write('music_album.json', JSON.pretty_generate(json_data))
     puts 'Music albums saved successfully!'
+  end
+
+  def save_movie
+    json_data = @movies.map do |movie|
+      {
+        name: movie.name,
+        publish_date: movie.publish_date,
+        silent: movie.silent,
+        source: movie.source,
+      }
+    end
+    File.write('movie.json', JSON.pretty_generate(json_data))
+    puts 'Movie saved successfully'
   end
 end
