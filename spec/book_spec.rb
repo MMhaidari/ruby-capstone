9b75b207f9ad023a30f38af6b7d437f09f37d236
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-06-22 00:44:10 +0530

    Linters fixed

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,28 +1,23 @@
 require_relative '../classes/book'
-require 'date'
 require 'rspec'
-require_relative '../classes/list_all_book_and_label'
 
 RSpec.describe Book do
   describe '#can_be_archived?' do
     it 'returns true if parent returns true' do
       book = Book.new('2001-01-01', 'No Starch Press', 'good', 'My Book')
       allow(book).to receive(:super).and_return(true)
-      test_method = book.can_be_archived?
-      expect(test_method).to eq(true)
+      expect(book.can_be_archived?).to eq(true)
     end
 
     it 'returns false if parent returns false' do
       book = Book.new('2022-01-01', 'No Starch Press', 'good', 'My Book')
       allow(book).to receive(:super).and_return(false)
-      test_method = book.can_be_archived?
-      expect(test_method).to eq(false)
+      expect(book.can_be_archived?).to eq(false)
     end
 
     it 'returns true if cover state is bad' do
       book = Book.new('2001-01-01', 'No Starch Press', 'bad', 'My Book')
-      test_method = book.can_be_archived?
-      expect(test_method).to eq(true)
+      expect(book.can_be_archived?).to eq(true)
     end
   end
 end
--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,13 +1,19 @@
 require_relative '../classes/label'
-require_relative '../classes/item'
-require_relative '../classes/list_all_book_and_label'
 require 'rspec'
 
 RSpec.describe Label do
+  describe '#initialize' do
+    it 'creates a label with the given title and color' do
+      label = Label.new('My Label', 'red')
+      expect(label.title).to eq('My Label')
+      expect(label.color).to eq('red')
+    end
+  end
+
   describe '#add_item' do
     it 'adds the item to the label' do
       label = Label.new('My Label', 'red')
-      item = Item.new('2001-01-01')
+      item = Item.new('2022-01-01')
       label.add_item(item)
       expect(label.items).to include(item)
     end
