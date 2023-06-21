41ae84ddb8fbad0403403e063902f1604972901a
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-06-21 17:34:54 +0530

    Fixing linter

--- classes/book.rb
+++ classes/book.rb
@@ -3,7 +3,7 @@ require_relative 'item'
 class Book < Item
   attr_reader :publisher, :cover_state, :title
 
-  def initialize(publish_date, publisher, cover_state, :title)
+  def initialize(publish_date, publisher, cover_state, title)
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
