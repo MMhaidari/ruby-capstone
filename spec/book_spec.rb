49126ad0df6b21f7ec99315fd05ddfd2371021c2
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-06-21 20:18:28 +0530

    Book and label test updated

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,44 +1,20 @@
-require_relative '../classes/book'
-require 'date'
 require 'rspec'
+require_relative '../classes/book'
 
-describe Book do
-  before :each do
-    @book = Book.new('2023-01-01', 'Example Publisher', 'good')
-  end
-
-  describe '#initialize' do
-    it 'sets the publish date, publisher, and cover state' do
-      expect(@book.publish_date).to eq('2023-01-01')
-      expect(@book.publisher).to eq('Example Publisher')
-      expect(@book.cover_state).to eq('good')
+RSpec.describe Book do
+  describe '#can_be_archived?' do
+    it 'returns true if parent returns true' do
+      book = Book.new('No Starch Press', 'good', '2001-01-01')
+      test_method = book.can_be_archived?
+      expect(test_method).to eq(true)
     end
   end
 
-  describe '#move_to_archive' do
-    context 'when the cover state is bad' do
-      before do
-        @book = Book.new('2023-01-01', 'Example Publisher', 'bad')
-      end
-
-      it 'marks the book as archived' do
-        @book.move_to_archive
-        expect(@book.instance_variable_get(:@archived)).to be true
-      end
-    end
-
-    context 'when the cover state is not bad' do
-      it 'marks the book as archived if it is more than 10 years old' do
-        allow(Time).to receive_message_chain(:now, :year).and_return(2034)
-        @book.move_to_archive
-        expect(@book.instance_variable_get(:@archived)).to be true
-      end
-
-      it 'does not mark the book as archived if it is less than 10 years old' do
-        allow(Time).to receive_message_chain(:now, :year).and_return(2024)
-        @book.move_to_archive
-        expect(@book.instance_variable_get(:@archived)).to be false
-      end
+  describe '#can_be_archived?' do
+    it 'returns true if parent returns true' do
+      book = Book.new('No Starch Press', 'good', '2022-01-01')
+      test_method = book.can_be_archived?
+      expect(test_method).to eq(false)
     end
   end
 end
