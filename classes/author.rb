511a7103358572e22e09600e9ecf988c6cbb6ef1
Rana-Mudassir <mduasire38@gmail.com>
2023-06-21 11:44:43 +0500

    Update linters

--- classes/author.rb
+++ classes/author.rb
@@ -4,8 +4,8 @@ class Author
   attr_reader :id
   attr_accessor :first_name, :last_name, :items
 
-  def initialize(id = rand(1..1000), first_name, last_name)
-    @id = id
+  def initialize(id, first_name, last_name)
+    @id = rand(1..1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
